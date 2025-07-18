9391455e838d26cac05163a8bd6a0256f777a131
rohit-002370 <rohitbangar1820@gmail.com>
2025-07-18 10:49:35 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+#taking a integer as input
+num=int(input("Enter a number: "))
+#check the number is even or odd
+if num%2==0:
+    print(num,"is an even")
+else:
+    print(num,"is an odd")
---
+++ Task2.py
@@ -0,0 +1,9 @@
+#initialize veriable to store sum
+totalsum=0
+#itaret from a (1,50)
+for number in range(1,50):
+    totalsum+=number
+#displaying final sum
+print("the sum of numbers from 1 to 50 is ",totalsum)
+
+
